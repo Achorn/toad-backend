a09878e43b2298be0044e3da975cc2a5205f25bb
achorn <joshuaachorn@gmail.com>
2024-01-25 13:20:51 -0800

    added toad routes

---
+++ routes/toad.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+//GET all toads
+router.get("/", (req, res) => {
+  res.json({ greeting: "Hello from get all toads route" });
+});
+
+router.get("/:id", (req, res) => {
+  res.json({ greeting: "Hello from get one toad route" });
+});
+
+router.post("/", (req, res) => {
+  res.json({ greeting: "Hello from POST a toad route" });
+});
+
+router.patch("/:id", (req, res) => {
+  res.json({ greeting: "Hello from update toad route" });
+});
+
+router.delete("/:id", (req, res) => {
+  res.json({ greeting: "Hello from delet a toad route" });
+});
