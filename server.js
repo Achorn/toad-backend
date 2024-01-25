8f7528f998609f026337f5d480b19ec2a5be3eb8
achorn <joshuaachorn@gmail.com>
2024-01-25 13:27:29 -0800

    added readme

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
-const toadRoutes = require("./routes/toad.js");
+const toadRoutes = require("./routes/toads.js");
 const app = express();
 
 app.use(express.json());
