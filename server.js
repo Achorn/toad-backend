11c2736e0fe19aefcaf163f1329933806dfd7b96
achorn <joshuaachorn@gmail.com>
2024-01-25 13:12:52 -0800

    added express server and database

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// app.use("api/toads", toadRoutes);
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  const listener = app.listen(process.env.PORT || 4000, () => {
+    console.log(
+      "Your app connected to db & is listening on port " +
+        listener.address().port
+    );
+  });
+});
