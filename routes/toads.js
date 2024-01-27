a1720dc572e2e725897d3f003222d564f046ce8a
achorn <joshuaachorn@gmail.com>
2024-01-27 12:07:24 -0800

    returning data instead of wrapping data in object

--- routes/toads.js
+++ routes/toads.js
@@ -5,11 +5,14 @@ const Toad = require("../models/toadModel");
 //GET all toads
 router.get("/", (req, res) => {
   Toad.find()
-    .then((data) => res.json({ data }))
+    .then((data) => res.json(data))
     .catch((err) => res.status(400).json(err.message));
 });
 
 router.get("/:id", (req, res) => {
+  Toad.findById(req.params.id)
+    .then((data) => json(data))
+    .catch((err) => {});
   res.json({ greeting: "Hello from get one toad route" });
 });
 
@@ -18,7 +21,7 @@ router.post("/", (req, res) => {
   console.log(toad);
   toad
     .save()
-    .then((data) => res.status(200).json({ data: data }))
+    .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json({ error: err.message }));
 });
 
