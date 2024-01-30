dbc5cd578744f0b4aec5dec290ac87bd397d6d18
achorn <joshuaachorn@gmail.com>
2024-01-30 15:10:04 -0800

    adding user controller

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
+const { signupUser, loginUser } = require("../controllers/userController");
+router.post("/signup", signupUser);
 
-router.post("/signup", () => {});
-
-router.post("/login", () => {});
+router.post("/login", loginUser);
 
 module.exports = router;
