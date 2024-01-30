08ab19bff6422c9df233e34d75be43f829080dee
achorn <joshuaachorn@gmail.com>
2024-01-30 09:55:04 -0800

    added toad controller to clean up toad routes file

---
+++ controllers/toadController.js
@@ -0,0 +1,20 @@
+const Toad = require("../models/toadModel");
+const mongoose = require("mongoose");
+
+const getToads = async (req, res) => {};
+
+const getToad = async (req, res) => {};
+
+const createToad = async (req, res) => {};
+
+const deleteToad = async (req, res) => {};
+
+const updateToad = async (req, res) => {};
+
+module.exports = {
+  getToads,
+  getToad,
+  createToad,
+  deleteToad,
+  updateToad,
+};
