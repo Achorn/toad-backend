78682182cea6a7f27df05ffde85a701405b31b21
achorn <joshuaachorn@gmail.com>
2024-01-31 14:13:58 -0800

    added jsonwebtoken to signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,17 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
   User.signup(email, password)
-    .then((data) => res.json(data))
+    .then((data) => {
+      const token = createToken(data._id);
+      res.json({ email, token });
+    })
     .catch((err) => res.json({ error: err.message }));
 };
 
