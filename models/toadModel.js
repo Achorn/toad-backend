95e7eb02d48f5e2b56994ce6a92b90f7ff13513e
achorn <joshuaachorn@gmail.com>
2024-02-09 10:08:48 -0800

    added half health for initial toad creation so user can feed and educate imediately

--- models/toadModel.js
+++ models/toadModel.js
@@ -3,8 +3,14 @@ const mongoose = require("mongoose");
 const toadSchema = mongoose.Schema({
   name: { type: String, required: true },
   user_id: { type: String, required: true, unique: true },
-  last_feeding: { type: Date, default: new Date() },
-  last_lesson: { type: Date, default: new Date() },
+  last_feeding: {
+    type: Date,
+    default: new Date().setHours(new Date().getHours() - 20),
+  },
+  last_lesson: {
+    type: Date,
+    default: new Date().setHours(new Date().getHours() - 20),
+  },
   last_activity: { type: Date, default: new Date() },
   education_points: { type: Number, default: 0 },
   water_points: { type: Number, default: 0 },
